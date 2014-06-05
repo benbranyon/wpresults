7718b3e0369efa53c41e5d4f1dadee9ebc704561
Ben Branyon <ben_branyon@yahoo.com>
2014-06-04 22:32:56 -0700

    update page template

--- wp-content/themes/wpresults/page.php
+++ wp-content/themes/wpresults/page.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<div class="row">
 			<div class="col-sm-6">
 			<?php while ( have_posts() ) : the_post(); ?>
-
+				<?php the_title( '<h1 class="entry-title">', '</h1>' );?>
 				<?php the_content(); ?>
 
 			<?php endwhile; ?>
