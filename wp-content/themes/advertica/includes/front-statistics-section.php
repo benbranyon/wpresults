d93db29d47cda64bf8b2d1c853431ed166c7ce21
Ben Branyon <ben_branyon@yahoo.com>
2014-09-18 00:23:59 -0700

    hide stats

--- wp-content/themes/advertica/includes/front-statistics-section.php
+++ wp-content/themes/advertica/includes/front-statistics-section.php
@@ -4,8 +4,8 @@ if($sktstatics_metabox == '1'){ ?>
 	<div class="container" >
 		<div class="row-fluid">
 			<div class="span12">
-				<?php $skt_staticsarea_section = get_post_meta( $post->ID,'_skt_staticsarea_section',true ); ?>
-				<?php if(isset($skt_staticsarea_section)) { echo do_shortcode($skt_staticsarea_section);} ?>				
+				<?php //$skt_staticsarea_section = get_post_meta( $post->ID,'_skt_staticsarea_section',true ); ?>
+				<?php //if(isset($skt_staticsarea_section)) { echo do_shortcode($skt_staticsarea_section);} ?>				
 			</div>
 		</div>
 	</div>
