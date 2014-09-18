c8271ace5eabfa5cade7663e33f536c8cc55e0ec
Ben Branyon <ben_branyon@yahoo.com>
2014-09-17 22:47:55 -0700

    add another row to featured section

--- wp-content/themes/advertica/includes/front-featured-boxes-section.php
+++ wp-content/themes/advertica/includes/front-featured-boxes-section.php
@@ -83,6 +83,31 @@ if($fraturedbox == '1'){?>
 			</div>
 			<div class="clearfix"></div>
 		</div>
+		<div class="mid-box-mid row-fluid"> 
+			<!-- Featured Box 3 -->
+			<div class="mid-box span4 fade_in_hide element_fade_in" >
+				<div class="skt-iconbox iconbox-top">		
+					<div class="iconbox-icon skt-animated small-to-large skt-viewport">
+					  <?php if(sketch_get_option($advertica_shortname.'_fb3_third_part_image')) { ?>			
+						<a class="skt-featured-images" href="<?php if(sketch_get_option($advertica_shortname."_fb3_third_part_link")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_link"); } ?>" title="<?php if(sketch_get_option($advertica_shortname."_fb3_third_part_heading")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_heading"); } ?>">				
+								<span class="skt-featured-image-mask"></span>
+								<img class="skin-bg" src="<?php  echo sketch_get_option($advertica_shortname.'_fb3_third_part_image','advertica'); ?>" alt="boximg"/>
+						</a>
+					  <?php } else { ?>
+						<a class="skt-featured-icons" href="<?php if(sketch_get_option($advertica_shortname."_fb3_third_part_link")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_link"); } ?>" title="<?php if(sketch_get_option($advertica_shortname."_fb3_third_part_heading")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_heading"); } ?>">				
+							<i class="fa <?php echo $_fb3_first_icon_class; ?>"></i>
+						</a>
+					  <?php } ?>	
+					</div>			
+					<div class="iconbox-content">			
+						<h4><?php if(sketch_get_option($advertica_shortname."_fb3_third_part_heading")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_heading"); } ?></h4>				
+						<p><?php if(sketch_get_option($advertica_shortname."_fb3_third_part_content")) { echo sketch_get_option($advertica_shortname."_fb3_third_part_content"); } ?></p>		
+					</div>		
+					<div class="clearfix"></div>	
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
 	</div>
 </div>
 <?php }?>
